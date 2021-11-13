e2135e828618795f51020b17b34b656231ef51ea
“the-tuts” <dwiastuti.work@gmail.com>
2021-11-13 08:39:36 +0700

    chore(): prettier

--- api-web-cypress/cypress/plugins/index.ts
+++ api-web-cypress/cypress/plugins/index.ts
@@ -2,7 +2,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
- export default (on: any, config: any) => {
+export default (on: any, config: any) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
